126b1a57b82141ddc4fe163d5666d11babdd665c
Maheshi <maheshianuradha2000@gmail.com>
2024-10-04 14:20:20 +0530

    first commit

---
+++ Todo-app/UserTextView.swift
@@ -0,0 +1,18 @@
+//
+//  UserTextView.swift
+//  Todo-app
+//
+//  Created by Maheshi Anuradha on 2024-10-04.
+//
+
+import SwiftUI
+
+struct UserTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserTextView()
+}
