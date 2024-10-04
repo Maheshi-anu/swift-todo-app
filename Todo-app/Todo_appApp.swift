e55bbfe29e021830ea63af69e5aa18741a1cbd5b
Maheshi <maheshianuradha2000@gmail.com>
2024-10-04 14:03:51 +0530

    Initial Commit

---
+++ Todo-app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo-app
+//
+//  Created by Maheshi Anuradha on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
